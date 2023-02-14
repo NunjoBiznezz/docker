3453d324e52aad537562c6735cb7b424d925fac0
doug <doug.bercot@gmail.com>
2023-02-14 16:38:35 -0500

    MySQL finally works

--- mysql/sql/init.sql
+++ mysql/sql/init.sql
@@ -1,3 +1,5 @@
 CREATE DATABASE keycloak;
-CREATE USER 'keycloak'@'%' IDENTIFIED WITH mysql_native_password BY 'password';
-GRANT ALL ON keycloak.* TO 'keycloak'@'%';
+# CREATE USER 'keycloak'@'%' IDENTIFIED WITH mysql_native_password BY 'password';
+# GRANT ALL ON keycloak.* TO 'keycloak'@'%';
+CREATE USER 'keycloak' IDENTIFIED WITH mysql_native_password BY 'password';
+GRANT ALL ON keycloak.* TO 'keycloak';
