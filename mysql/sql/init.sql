11d46beba68b0542396f3c7a9f1b7ee19d7132e0
doug <doug.bercot@gmail.com>
2023-02-14 16:11:00 -0500

    MySQL finally works

--- mysql/sql/init.sql
+++ mysql/sql/init.sql
@@ -1,7 +1,3 @@
-CREATE DATABASE playgolf;
-CREATE USER 'user'@'%' IDENTIFIED WITH mysql_native_password BY 'password';
-GRANT ALL ON playgolf.* TO 'user'@'%';
-
 CREATE DATABASE keycloak;
 CREATE USER 'keycloak'@'%' IDENTIFIED WITH mysql_native_password BY 'password';
 GRANT ALL ON keycloak.* TO 'keycloak'@'%';
