815e03eeac4f26bcca1592b364716be99c616cc3
doug <doug.bercot@gmail.com>
2023-02-15 16:45:11 -0500

    Traefix up

---
+++ mongodb/import/import.sh
@@ -0,0 +1,1 @@
+mongoimport -u nunjo -p dougals mongodb://mongodb:27017 --db play_golf --collection users --drop --file /data/mongodb-import/users.json --jsonArray
--- mongodb/init/1-init-playgolf.js
+++ mongodb/init/1-init-playgolf.js
@@ -13,9 +13,9 @@ db = new Mongo().getDB("play_golf");
 
 // Initialize empty collections just to see that this works...
 db.createCollection('users', { capped: false });
-db.users.insertOne({
-   nickName: 'Fresh', firstName: 'Doug', lastName: 'Bercot', phone: '2604468802', email: 'doug.bercot@gmail.com'
-})
+// db.users.insertOne({
+//    nickName: 'Fresh', firstName: 'Doug', lastName: 'Bercot', phone: '2604468802', email: 'doug.bercot@gmail.com'
+// })
 
 db.createCollection('courses', { capped: false });
 db.createCollection('events', { capped: false });
